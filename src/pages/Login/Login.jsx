1a797d6d2a6a789782736d6745bbead529e962d4
VilelaMarcus <marcusvilela000@gmail.com>
2024-02-07 22:58:01 -0300

    Commit Login Implementado
    Autor: Marcus Vilela
    
    Data: 07/02/2024
    
    Componente de login implementado, agora esta faltando implementar navegação
    e criar a tela de chat em si

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,89 +1,38 @@
-import { useState } from "react";
+import  { useState } from "react";
 import TextField from '@mui/material/TextField';
-import logoUrl from '../../assets/images/default-avatar.png';
-import { IconYellow } from '../../assets/Icons/edit-icon.jsx';
-// import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-// import { auth, db, storage } from "../../db.js";
-// import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-// import { doc, setDoc } from "firebase/firestore";
-// import { useNavigate } from "react-router-dom";
 
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../db.js";
 import {   
+
     FormContainer, 
-    ProfileImage, 
     RegisterButton, 
-    RegisterContent, 
-    ImgContainer, 
+    RegisterContent,  
     SubTitle, 
     Title, 
-    ImageOverlay,
     InputStyleForm, 
-    ProfileImageWrapper,
-    IconWrapper,
-    Input2
 } from "./Login.styles.js";
 
 
-const Register = () => {
-  // const [err, setErr] = useState(false);
-  // const [loading, setLoading] = useState(false);
-  // const navigate = useNavigate();
+const Login = () => {
+  const [err, setErr] = useState(false);
 
   const handleSubmit = async (e) => {
-    // setLoading(true);
     e.preventDefault();
-    const displayName = e.target[0].value;
-    const email = e.target[1].value;
-    const password = e.target[2].value;
-    const confirmedPassword = e.target[3].value;
-    
-    console.log(displayName, email, password, confirmedPassword);
+    const email = e.target[0].value;
+    const password = e.target[1].value;
 
-    // try {
-    //   //Create user
-    //   const res = await createUserWithEmailAndPassword(auth, email, password);
-
-    //   //Create a unique image name
-    //   const date = new Date().getTime();
-    //   const storageRef = ref(storage, `${displayName + date}`);
-
-    //   await uploadBytesResumable(storageRef, file).then(() => {
-    //     getDownloadURL(storageRef).then(async (downloadURL) => {
-    //       try {
-    //         //Update profile
-    //         await updateProfile(res.user, {
-    //           displayName,
-    //           photoURL: downloadURL,
-    //         });
-    //         //create user on firestore
-    //         await setDoc(doc(db, "users", res.user.uid), {
-    //           uid: res.user.uid,
-    //           displayName,
-    //           email,
-    //           photoURL: downloadURL,
-    //         });
-
-    //         //create empty user chats on firestore
-    //         await setDoc(doc(db, "userChats", res.user.uid), {});
-    //         navigate("/");
-    //       } catch (err) {
-    //         console.log(err);
-    //         setErr(true);
-    //         setLoading(false);
-    //       }
-    //     });
-    //   });
-    // } catch (err) {
-    //   setErr(true);
-    //   setLoading(false);
-    // }
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      setErr(true);
+    }
   };
 
     return (
         <RegisterContent>
             <FormContainer onSubmit={handleSubmit}>
                 <Title>Bem Vindo 👋</Title>  
-                <SubTitle>Converse e conecte-se em tempo real</SubTitle>  
                 <TextField 
                     label="Nome de Usuario" 
                     variant="filled" 
@@ -114,4 +63,4 @@ const Register = () => {
     );
 };
 
-export default Register;
+export default Login;
